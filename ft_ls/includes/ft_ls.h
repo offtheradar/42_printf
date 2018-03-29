0ca43daf44ad66943cb05828b6ab46547325243d
Yacine Sibous <yacinesibous@gmail.com>
2018-03-28 21:42:16 -0700

    attempt to construct a tree

--- ft_ls/includes/ft_ls.h
+++ ft_ls/includes/ft_ls.h
@@ -6,12 +6,13 @@
 /*   By: ysibous <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 21:01:29 by ysibous           #+#    #+#             */
-/*   Updated: 2018/03/28 15:42:35 by ysibous          ###   ########.fr       */
+/*   Updated: 2018/03/28 21:08:52 by ysibous          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # include "libft/libft.h"
+# include <sys/stat.h>
 # include <dirent.h>
 
 typedef struct		s_options
@@ -23,9 +24,19 @@ typedef struct		s_options
 	int		t;
 }					t_options;
 
-void				init_options(t_options *opt);
+typedef	struct		s_file_info
+{
+	char	*name;
+	int		is_hidden;
+	int		is_dir;
+	t_list	*files;
+}					t_file_info;
+
+t_options			*init_options(void);
 
 void				set_options(char *options, t_options *opt);
 
 int					get_options(int argc, char **argv, t_options *opt);
+
+void				get_file_info(char *str, t_list *lst);
 #endif
--- ft_ls/src/main.c
+++ ft_ls/src/main.c
@@ -6,29 +6,65 @@
 /*   By: ysibous <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/26 20:22:50 by ysibous           #+#    #+#             */
-/*   Updated: 2018/03/28 15:50:19 by ysibous          ###   ########.fr       */
+/*   Updated: 2018/03/28 21:41:57 by ysibous          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_ls.h"
 #include <stdio.h>
 
+void	get_file_info(char *str, t_list *lst)
+{
+	struct stat *buff;
+	t_file_info	*info;
+	t_file_info *new;
+	DIR			*d;
+	struct dirent	*dir;
+
+	info = (t_file_info *)ft_memalloc(sizeof(t_file_info));
+	new = (t_file_info *)ft_memalloc(sizeof(t_file_info));
+	buff = (struct stat *)ft_memalloc(sizeof(struct stat));
+	dir = (struct dirent *)ft_memalloc(sizeof(struct dirent));
+	lstat(str, buff);
+	info->name = str;
+	ft_lst_add_to_end(ft_lstnew(info, sizeof(t_file_info)), &lst);
+	if (S_ISDIR(buff->st_mode))
+	{
+		d = opendir(str);
+		while ((dir = readdir(d)))
+		{
+			new->name = dir->d_name;
+			ft_lst_add_to_end(ft_lstnew(new, sizeof(t_file_info)), &(info->files));
+			free(new);
+		}
+		closedir(d);
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	int  i;
+	t_list *lst;
 	//DIR *d;
 	//struct dirent *dir;
 	t_options *opt;
-	opt = NULL;
-	init_options(opt);
-	i = 1;
+	opt = init_options();
+	//t_list	*lst;
 /*
 	if (argc == 1)
 		d = opendir(".");
 	else
 		d = opendir(argv[1]);
 */
-	get_options(argc, argv, opt);
+	//lst = ft_lstnew(0,0);
+	lst = (t_list *)ft_memalloc(sizeof(t_list));
+	i = get_options(argc, argv, opt);
+	while (i < argc)
+	{
+		get_file_info(argv[i], lst);
+		i++;
+	}
+//	print_lst(lst);
 /*
 	if (d)
 	{
@@ -39,6 +75,11 @@ int		main(int argc, char **argv)
 		closedir(d);
 	}
 */
+	while (lst)
+	{
+		printf("file name is %s\n", lst->content->name);
+		lst = lst->next;
+	}
 	printf("l is %d\n", opt->l);
 	printf("R is %d\n", opt->R);
 	printf("a is %d\n", opt->a);
